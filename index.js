f9a81b5b27848ce02a41b9b773f709e48a57ea92
Tremaine McKinley <49395726+tremckinley@users.noreply.github.com>
2025-09-21 12:43:06 +0000

    added get route

--- index.js
+++ index.js
@@ -28,3 +28,14 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.get("/api/whoami/", (req, res) => {
+  console.log(req.headers)
+  res.json({
+    'ipaddress': req.headers['x-real-ip'],
+    'language': req.headers['accept-language'],
+    'software': req.headers['user-agent']
+  })
+
+})
